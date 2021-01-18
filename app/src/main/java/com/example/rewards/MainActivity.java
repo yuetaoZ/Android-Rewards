ac834a8c2ff3e5704de22dd3ec1508ee0d29d9f6
yuetaoZ <yuetao.zhu.062984@gmail.com>
2021-01-18 13:02:14 -0600

    Changed Menu bar colors, added icon for Menu bar.

--- app/src/main/java/com/example/rewards/MainActivity.java
+++ app/src/main/java/com/example/rewards/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.icon);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
     }
 }
