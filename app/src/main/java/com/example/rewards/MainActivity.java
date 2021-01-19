9af5d0a9461805762413ccc8a9dca13efb39bcd1
yuetaoZ <yuetao.zhu.062984@gmail.com>
2021-01-19 17:24:27 -0600

    Added activity_api_key.xml and APIInfoDialogFragment.java for API Key dialog.

--- app/src/main/java/com/example/rewards/MainActivity.java
+++ app/src/main/java/com/example/rewards/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.rewards;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.Objects;
 
@@ -13,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupActionBar();
+        checkAPIInfo(savedInstanceState);
+    }
+
+    private void checkAPIInfo(Bundle savedInstanceState) {
+        APIInfoDialogFragment apiDialogFragment = new APIInfoDialogFragment();
+        apiDialogFragment.show(getSupportFragmentManager(), "apikey");
+    }
+
+    private void setupActionBar() {
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.icon);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
