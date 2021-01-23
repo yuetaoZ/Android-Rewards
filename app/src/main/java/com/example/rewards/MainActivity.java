8f47f7b23414fb0444c250f60964072fd07a705b
yuetaoZ <yuetao.zhu.062984@gmail.com>
2021-01-22 18:39:17 -0600

    Finished clearSavedAPIKeyButton, info dialog for save and clear API key.

--- app/src/main/java/com/example/rewards/MainActivity.java
+++ app/src/main/java/com/example/rewards/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.rewards;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    private String APIFirstName;
-    private String APILastName;
-    private String APIStdID;
-    private String APIStdEmail;
-    AlertDialog API_dialog;
+    private String APIFirstName = "";
+    private String APILastName = "";
+    private String APIStdID = "";
+    private String APIStdEmail = "";
+    private String APIKey = "";
+    private boolean APIKeySaved = false;
+    private AlertDialog API_dialog;
+
+    private static final String SHARED_PREFS = "sharedPrefs";
+    private static final String FIRSTNAME = "firstName";
+    private static final String LASTNAME = "lastName";
+    private static final String STUDENT_EMAIL = "studentEmail";
+    private static final String STUDENT_ID = "studentID";
+    private static final String APIKEY = "studentAPIKey";
+    private static final String APIKEY_SAVED = "APIKEYSaved";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +36,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupActionBar();
-
+        setupClearAPIKeyButton();
         setupAPI_Dialog();
+    }
 
+    private void setupClearAPIKeyButton() {
+        Button clearAPIKeyBtn = findViewById(R.id.clearAPIKeyButton);
+        clearAPIKeyBtn.setOnClickListener(v -> {
+            clearSavedAPI();
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("API Key Cleared")
+                    .setMessage("API Key cleared successfully!")
+                    .setIcon(R.drawable.logo)
+                    .setPositiveButton("OK", (dialog, which) -> setupAPI_Dialog())
+                    .show();
+        });
     }
 
     private void setupAPI_Dialog() {
+        loadData();
+        if (APIKeySaved) return;
         LayoutInflater inflater = LayoutInflater.from(this);
         final View customView = inflater.inflate(R.layout.activity_api_key, null);
 
-        final TextView firstName = (EditText) customView.findViewById(R.id.API_firstname);
-        final TextView lastName = (EditText) customView.findViewById(R.id.API_lastname);
-        final TextView stdEmail = (EditText) customView.findViewById(R.id.API_studentEmail);
-        final TextView stdID = (EditText) customView.findViewById(R.id.API_studentID);
+        final TextView firstName = customView.findViewById(R.id.API_firstname);
+        final TextView lastName = customView.findViewById(R.id.API_lastname);
+        final TextView stdEmail = customView.findViewById(R.id.API_studentEmail);
+        final TextView stdID = customView.findViewById(R.id.API_studentID);
 
-        if (APIFirstName != null) firstName.setHint(APIFirstName);
-        if (APILastName != null) lastName.setHint(APILastName);
-        if (APIStdEmail != null) stdEmail.setHint(APIStdEmail);
-        if (APIStdID != null) stdID.setHint(APIStdID);
+        if (!APIFirstName.equals("")) firstName.setHint(APIFirstName);
+        if (!APILastName.equals("")) lastName.setHint(APILastName);
+        if (!APIStdEmail.equals("")) stdEmail.setHint(APIStdEmail);
+        if (!APIStdID.equals("")) stdID.setHint(APIStdID);
 
         API_dialog = new AlertDialog.Builder(this)
                 .setView(customView)
@@ -56,10 +82,10 @@ public class MainActivity extends AppCompatActivity {
                     validateInputs();
                 })
                 .setNegativeButton("Cancel", (dialog12, which) -> {
-                    APIFirstName = null;
-                    APILastName = null;
-                    APIStdEmail = null;
-                    APIStdID = null;
+                    APIFirstName = "";
+                    APILastName = "";
+                    APIStdEmail = "";
+                    APIStdID = "";
                 }).create();
 
         API_dialog.show();
@@ -72,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
             String email = APIStdEmail.trim();
             String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\." + "edu";
             if (email.matches(emailPattern)) {
+                saveData();
                 showAPISuccessDialog();
             } else {
                 showWrongEmailDialog();
@@ -80,9 +107,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showMissingDataDialog() {
+        String message = "";
+        if (APIFirstName.equals("")) message += "Missing data: First Name.\n";
+        if (APILastName.equals("")) message += "Missing data: Last Name.\n";
+        if (APIStdEmail.equals("")) message += "Missing data: Student Email.\n";
+        if (APIStdID.equals("")) message += "Missing data: Student ID.\n";
         new AlertDialog.Builder(this)
                 .setTitle("Invalid request")
-                .setMessage("Missing data")
+                .setMessage(message)
                 .setIcon(R.drawable.logo)
                 .setPositiveButton("OK", (dialog1, which) -> API_dialog.show())
                 .show();
@@ -90,8 +122,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void showWrongEmailDialog() {
         new AlertDialog.Builder(this)
-                .setTitle("Invalid request")
-                .setMessage("Non-edu email address")
+                .setTitle("Invalid email address")
+                .setMessage("Not a valid edu email address.")
                 .setIcon(R.drawable.logo)
                 .setPositiveButton("OK", (dialog1, which) -> API_dialog.show())
                 .show();
@@ -117,5 +149,36 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayUseLogoEnabled(true);
     }
 
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
 
+        editor.putString(FIRSTNAME, APIFirstName);
+        editor.putString(LASTNAME, APILastName);
+        editor.putString(STUDENT_EMAIL, APIStdEmail);
+        editor.putString(STUDENT_ID, APIStdID);
+        editor.putString(APIKEY, APIKey);
+        editor.putBoolean(APIKEY_SAVED, !APIFirstName.equals(""));
+
+        editor.apply();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        APIFirstName = sharedPreferences.getString(FIRSTNAME, "");
+        APILastName = sharedPreferences.getString(LASTNAME, "");
+        APIStdEmail = sharedPreferences.getString(STUDENT_EMAIL, "");
+        APIStdID = sharedPreferences.getString(STUDENT_ID, "");
+        APIKey = sharedPreferences.getString(APIKEY, "");
+        APIKeySaved = sharedPreferences.getBoolean(APIKEY_SAVED, false);
+    }
+
+    public void clearSavedAPI() {
+        APIFirstName = "";
+        APILastName = "";
+        APIStdEmail = "";
+        APIStdID = "";
+        APIKey = "";
+        saveData();
+    }
 }
