1323dc35ce432e8684330ebefd9948f6a3b2d802
yuetaoZ <yuetao.zhu.062984@gmail.com>
2021-01-25 10:53:42 -0600

    Create Profile class.

---
+++ app/src/main/java/com/example/rewards/Profile.java
@@ -0,0 +1,18 @@
+package com.example.rewards;
+
+import org.json.JSONArray;
+
+public class Profile {
+    String firstName,
+            lastName,
+            userName,
+            department,
+            story,
+            position,
+            password,
+            remainingPointsToAward,
+            location,
+            imageBytes;
+    JSONArray rewardRecordViews;
+
+}
